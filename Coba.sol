2575c49d706bb613f493bb358ef3c08bf1d3b40f
Sonic1-Max <sonicofficialcoin@gmail.com>
2025-03-12 09:46:32 +0800

    Update Coba.sol

--- Coba.sol
+++ Coba.sol
@@ -1,75 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-}
+contract OtakuDump is ERC20 {
+    uint256 constant TOTAL_SUPPLY = 121_000_000_000_000 * 10**18;
+    uint256 constant FEE = 100; // 1% fee (basis poin)
 
-contract OtakuDump is IERC20 {
-    string public constant name = "OtakuDump";
-    string public constant symbol = "OTD";
-    uint8 public constant decimals = 18;
-    uint256 private immutable _totalSupply = 121 * 10**12 * 10**18; // 121 Triliun
-
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    constructor() {
-        _balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+    constructor() ERC20("OtakuDump", "OTD") {
+        _mint(msg.sender, TOTAL_SUPPLY);
     }
 
-    function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) external view override returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(address recipient, uint256 amount) external override returns (bool) {
-        uint256 senderBalance = _balances[msg.sender];
-        require(senderBalance >= amount, "Insufficient balance");
-
-        unchecked {
-            _balances[msg.sender] = senderBalance - amount;
-        }
-        _balances[recipient] += amount;
-
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) external view override returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 amount) external override returns (bool) {
-        _allowances[msg.sender][spender] = amount;
-        return true;
-    }
-
-    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
-        uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "Insufficient balance");
-
-        uint256 currentAllowance = _allowances[sender][msg.sender];
-        require(currentAllowance >= amount, "Allowance exceeded");
-
-        unchecked {
-            _balances[sender] = senderBalance - amount;
-            _allowances[sender][msg.sender] = currentAllowance - amount;
-        }
-        _balances[recipient] += amount;
-
-        emit Transfer(sender, recipient, amount);
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        uint256 feeAmount = (amount * FEE) / 10000;
+        _transfer(msg.sender, address(this), feeAmount); // Fee ke kontrak
+        _transfer(msg.sender, recipient, amount - feeAmount);
         return true;
     }
 }
