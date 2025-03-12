f6f35f759018b02e76be2b76289fa76fd7d1555e
Sonic1-Max <sonicofficialcoin@gmail.com>
2025-03-12 09:52:14 +0800

    Update Coba.sol

--- Coba.sol
+++ Coba.sol
@@ -4,35 +4,35 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OtakuDump is ERC20, Ownable {
-    uint256 constant TOTAL_SUPPLY = 121_000_000_000_000 * 10**18;
-    uint256 constant FEE = 100; // 1% fee (basis poin)
-    uint256 private _totalBurned;
+    uint256 public constant TOTAL_SUPPLY = 121_000_000_000_000 * 10**18;
+    uint256 public constant FEE = 100; // 1% fee (basis poin)
+    uint256 private totalBurned;
+
+    event FeeBurned(address indexed burner, uint256 amount);
 
     constructor() ERC20("OtakuDump", "OTD") {
         _mint(msg.sender, TOTAL_SUPPLY);
-        transferOwnership(msg.sender); // Pastikan ownership jelas
+        renounceOwnership(); // Menghapus risiko rug pull
     }
 
     function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(recipient != address(0), "OtakuDump: transfer to zero address");
+        require(amount > 0, "OtakuDump: transfer amount must be greater than zero");
+
         uint256 feeAmount = (amount * FEE) / 10000;
         uint256 transferAmount = amount - feeAmount;
-        
+
         if (feeAmount > 0) {
-            _burn(msg.sender, feeAmount); // Burn fee langsung
-            _totalBurned += feeAmount;
+            _burn(msg.sender, feeAmount);
+            totalBurned += feeAmount;
+            emit FeeBurned(msg.sender, feeAmount);
         }
+
         _transfer(msg.sender, recipient, transferAmount);
         return true;
     }
 
-    // Fungsi untuk mengecek total burned
-    function totalBurned() public view returns (uint256) {
-        return _totalBurned;
-    }
-
-    // Hanya owner yang bisa menarik token (jika perlu)
-    function emergencyWithdraw(uint256 amount) public onlyOwner {
-        _transfer(address(this), msg.sender, amount);
+    function totalBurnedAmount() public view returns (uint256) {
+        return totalBurned;
     }
 }
